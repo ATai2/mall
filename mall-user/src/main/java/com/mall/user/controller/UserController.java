ecf5a1fc3a44206b958f335a10fd59d0557a8e0b
ATai2 <1114677814@qq.com>
2020-03-28 11:13:10 +0800

    ss

--- mall-user/src/main/java/com/mall/user/controller/UserController.java
+++ mall-user/src/main/java/com/mall/user/controller/UserController.java
@@ -2,13 +2,16 @@ package com.mall.user.controller;
 
 import com.mall.user.entity.UserEntity;
 import com.mall.user.repository.UserPrository;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Api(tags = "UserController", description = "UserController")
 @RestController
 public class UserController {
 
@@ -22,7 +25,7 @@ public class UserController {
     }
 
     @PostMapping("/user")
-    public UserEntity insert(UserEntity userEntity) {
+    public UserEntity insert(@RequestBody UserEntity userEntity) {
         UserEntity save = userPrository.save(userEntity);
         return save;
     }
